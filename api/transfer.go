5e712722bf6d32294c426430f64c0a4964e508f0
바빵이 <metalhwal@gmail.com>
2025-08-11 17:00:19 +0900

    feat: transfer create

---
+++ api/transfer.go
@@ -0,0 +1,115 @@
+package api
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+
+	db "github.com/duodecanol/simplebank/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+type TransferRequest struct {
+	FromAccountID int64  `json:"from_account_id" binding:"required,min=1"`
+	ToAccountID   int64  `json:"to_account_id" binding:"required,min=1"`
+	Amount        int64  `json:"amount" binding:"required,gt=0"`
+	Currency      string `json:"currency" binding:"required,oneof=USD EUR CAD"`
+}
+
+func (server *Server) createTransfer(ctx *gin.Context) {
+	var req TransferRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	if !server.validateAccount(ctx, req.FromAccountID, req.Currency) {
+		return
+	}
+	if !server.validateAccount(ctx, req.ToAccountID, req.Currency) {
+		return
+	}
+	// TODO: check if the from account balance is sufficient
+
+	arg := db.TransferTxParams{
+		FromAccountID: req.FromAccountID,
+		ToAccountID:   req.ToAccountID,
+		Amount:        req.Amount,
+	}
+	result, err := server.store.TransferTx(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, result)
+}
+func (server *Server) validateAccount(ctx *gin.Context, accountId int64, currency string) bool {
+	account, err := server.store.GetAccount(ctx, accountId)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return false
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return false
+	}
+	if account.Currency != currency {
+		err := fmt.Errorf("account [%d] currency mismatch: %s vs %s", account.ID, account.Currency, currency)
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return false
+	}
+
+	return true
+}
+
+type GetTransferRequest struct {
+	ID int64 `uri:"id" binding:"required,min=1"`
+}
+
+func (server *Server) getTransfer(ctx *gin.Context) {
+	var req GetTransferRequest
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	transfer, err := server.store.GetTransfer(ctx, req.ID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, transfer)
+}
+
+type ListTransferRequest struct {
+	PageID   int32 `form:"page_id" binding:"required,min=1"`
+	PageSize int32 `form:"page_size" binding:"required,min=5,max=10"`
+}
+
+func (server *Server) listTransfers(ctx *gin.Context) {
+	var req ListTransferRequest
+	if err := ctx.ShouldBindQuery(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	arg := db.ListAccountsParams{
+		Limit:  req.PageSize,
+		Offset: (req.PageID - 1) * req.PageSize,
+	}
+
+	accounts, err := server.store.ListAccounts(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, accounts)
+}
